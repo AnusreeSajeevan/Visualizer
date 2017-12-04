3685469d4e87ca6a7c0486637424dd56f8eab4cd
Anusree Sajeevan <anusreesajeevan111@gmail.com>
2017-12-04 18:34:39 +0530

    feat : Set preference summary

--- app/src/main/java/com/example/anu/visualizerapp/SettingsFragment.java
+++ app/src/main/java/com/example/anu/visualizerapp/SettingsFragment.java
@@ -1,20 +1,87 @@
 package com.example.anu.visualizerapp;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.net.Uri;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.preference.CheckBoxPreference;
+import android.support.v7.preference.ListPreference;
+import android.support.v7.preference.Preference;
 import android.support.v7.preference.PreferenceFragmentCompat;
+import android.support.v7.preference.PreferenceScreen;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class SettingsFragment extends PreferenceFragmentCompat {
+public class SettingsFragment extends PreferenceFragmentCompat implements
+        SharedPreferences.OnSharedPreferenceChangeListener{
 
 
     @Override
     public void onCreatePreferences(Bundle savedInstanceState, String rootKey) {
         addPreferencesFromResource(R.xml.pref_visualizer);
+
+        /**
+         * get the preference screen to get the number of preferences
+         * iterate through all the preferences and set the summary by calling the
+         * setSummary method by pasing preference and its value
+         */
+        SharedPreferences sharedPreferences = getPreferenceScreen().getSharedPreferences();
+        PreferenceScreen preferenceScreen = getPreferenceScreen();
+        int count = preferenceScreen.getPreferenceCount();
+        for (int i=0;i<count;i++){
+            Preference preference = preferenceScreen.getPreference(i);
+            if (!(preference instanceof CheckBoxPreference)){
+                setPreferenceSummary(preference, sharedPreferences.getString(getResources().getString(R.string.pref_color_key), ""));
+            }
+        }
+    }
+
+    /**
+     * method to set the user preferred preference value
+     * @param preference the preference whose value is to be updated
+     * @param value the user prefered value which is to be set
+     */
+    private void setPreferenceSummary(Preference preference, String value) {
+        if (null != preference){
+           if (preference instanceof ListPreference){
+               ListPreference listPreference = (ListPreference) preference;
+               int index = listPreference.findIndexOfValue(value);
+               if (index>=0){
+                    listPreference.setSummary(listPreference.getEntries()[index]);
+               }
+           }
+        }
+    }
+
+    @Override
+    public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+        Preference preference = findPreference(key);
+        if (null != preference){
+            if (!(preference instanceof CheckBoxPreference)){
+                setPreferenceSummary(preference, sharedPreferences.getString(key, ""));
+            }
+        }
+    }
+
+    /**
+     * register OnSharedPreferenceChangedListener
+     * @param savedInstanceState
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getPreferenceScreen().getSharedPreferences().registerOnSharedPreferenceChangeListener(this);
+    }
+
+    /**
+     * unregister OnSharedPreferenceChangedListener
+     */
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        getPreferenceScreen().getSharedPreferences().unregisterOnSharedPreferenceChangeListener(this);
     }
 }
